f9b373833ae2220f33ff3792679013b21bb168cf
Mulyono Putra <mulyonoputra1994@gmail.com>
2022-01-19 11:12:33 +0700

    404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './main/pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
   },
+  {
+    path: '404',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
