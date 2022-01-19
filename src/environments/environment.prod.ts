35e15e80fbb4ff1d56fbdb4e4e751dacd38ae54f
Mulyono Putra <mulyonoputra1994@gmail.com>
2022-01-19 11:34:57 +0700

    Test deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  current: 'http://api.openweathermap.org/data/2.5/weather?',
+  forecast: 'http://api.openweathermap.org/data/2.5/forecast?',
+  apiKey: '61aea816d83dc6c3604805fc5790f007'
 };
